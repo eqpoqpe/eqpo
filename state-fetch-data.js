9fb966b9da9bbd90fe4674e8aef2560c14ba0c05
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-08-29 11:05:06 +0800

    Update state-fetch-data.js

--- state-fetch-data.js
+++ state-fetch-data.js
@@ -10,18 +10,22 @@ export default function TrapButton({ setContext }) {
   const [onFetchData, setOnClick] = naiveEvent("click");
   const [fetcState, setFetchState] = naiveTemplateStore(false);
   
+  const FetchData = naiveFocus((setTemplate) => {
+    fetch("https://eqpo.ml/display.json", { methods: "GET" })
+      .then((response) => response.json())
+      .then((result) => {
+        setContext((_, chilren) => {
+          children.insert([JSON.stringify(result)]);
+        });
+    
+        setTemplate((prev) => !prev);
+      });
+  });
+  
   const SP_FETCH = () => {
     if (!fetchState.ready) {
       setFetchState((prev) => !prev);
-      fetch("https://eqpo.ml/display.json", { methods: "GET" })
-        .then((response) => response.json())
-        .then((result) => {
-          setContext((_, chilren) => {
-            children.insert([JSON.stringify(result)]);
-          });
-        
-          setFetchState((prev) => !prev);
-        });
+      FetchData(setFetchState);
     }
   };
   
