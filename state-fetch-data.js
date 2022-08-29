91f433715e32749d9a7316c525b8e3dc62f021c6
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-08-29 10:11:52 +0800

    Create state-fetch-data.js

---
+++ state-fetch-data.js
@@ -0,0 +1,32 @@
+import { naiveCreate, naiveEvent, naiveFocus, naiveTemplateStore } from "tinreact";
+
+export default function TrapButton({ setContext }) {
+  const [product, setProduct] = naiveCreate("div");
+  const [onFetchData, setOnClick] = naiveEvent("click");
+  const [fetcState, setFetchState] = naiveTemplateStore(false);
+  
+  const SP_FETCH = () => {
+    if (!fetchState) {
+      fetch("https://eqpo.ml/display.json", { methods: "GET" })
+        .then((response) => response.json())
+        .then((result) => {
+          setContext((_, chilren) => {
+            children.insert([JSON.stringify(result)]);
+          });
+        
+          setFetchState((prev) => !prev);
+        });
+    }
+  };
+  
+  setOnClick(SP_FETCH);
+  
+  setProduct((props, chilren) => {
+    props.class = "trap-button";
+    props.onClick = onFetchData;
+    
+    children.insert(["Click Me"]);
+  });
+  
+  return product;
+}
