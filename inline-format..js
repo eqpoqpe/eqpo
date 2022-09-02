b4c9cfb648685ff104f30fbd22b31d94cea39502
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-09-02 15:09:19 +0800

    Create inline-format..js

---
+++ inline-format..js
@@ -0,0 +1,14 @@
+const _el = `
+  div
+  :class=init-page
+  :id=inline-format-page
+  >
+    p
+    :class=init-title
+    |
+    p
+    :class=init-title
+    |
+    div
+    :class=wait-load-front
+`;
