5998993c629907c90ad8bbd095e0cf9bd0d0128c
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-09-02 15:10:32 +0800

    Update inline-format..js

--- inline-format..js
+++ inline-format..js
@@ -1,14 +1,8 @@
 const _el = `
-  div
-  :class=init-page
-  :id=inline-format-page
-  >
-    p
-    :class=init-title
+  div:class=init-page:id=inline-format-page>
+    p:class=init-title
     |
-    p
-    :class=init-title
+    p:class=init-title
     |
-    div
-    :class=wait-load-front
-`;
+    div:class=wait-load-front
+`
