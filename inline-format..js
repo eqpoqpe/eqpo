043767b45c02d4aa4f38af0b99fae5cbf129ebdb
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-09-02 15:17:08 +0800

    Update inline-format..js

--- inline-format..js
+++ inline-format..js
@@ -1,8 +1,14 @@
 const _el = `
-  div:class=init-page:id=inline-format-page>
-    p:class=init-title
+  div:class=init-page:id=inline-format-page
+    >
+      p:class=init-title
+      |
+      p:class=init-title
+      |
+      div:class=wait-load-front
+  div
+  >
+    p:text=free man here
     |
-    p:class=init-title
-    |
-    div:class=wait-load-front
+    p:text=free man there
 `
