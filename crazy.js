70ccfc4785005d60494a0345fdc1ab7c840acad7
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-09-20 22:43:14 +0800

    Update crazy.js

--- crazy.js
+++ crazy.js
@@ -1,11 +1,18 @@
-import { naiveCreate, naiveState } from "tinreact";
+import {
+  naiveCreate,
+  naiveState,
+  createElement
+} from "tinreact";
 
 function Trap() {
   const [product, setProduct] = naiveCreate("div");
   const [ready, setReadyValue] = naiveState(0);
+  const tips = createElement("div", [
+    createElement("p", [ready()])
+  ]);
   
   setProduct((props, children) => {
-    children.insert([ready()])
+    children.insert([tips])
   });
   
   return product;
